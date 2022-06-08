8cedc45da7a345b8bb9703e0b54742ab04185c60
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-08 21:10:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function saturdayFun(toDo = "roller-skate"){
+        return `This Saturday, I want to ${toDo}!`;
+    }
+
+let mondayWork = function(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}!`;
+}
